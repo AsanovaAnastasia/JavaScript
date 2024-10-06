34497a9706104a47a0bdc93ed5462018f78a7f6f
Asanova Anastasia <bisiurko.anastasia@gmail.com>
2024-10-06 20:56:51 +0300

    Homework1

---
+++ script.js
@@ -0,0 +1,9 @@
+let celsius = 17;
+let fahrenheit = (9 / 5) * celsius + 32;
+console.log('Температура в градусах Цельсия:', celsius);
+console.log('Температура в градусах Фаренгейта:', fahrenheit);
+
+
+let name = `Anastasia`;
+let admin = name;
+console.log('Значение переменной admin:', admin);
